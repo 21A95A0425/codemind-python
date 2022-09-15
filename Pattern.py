41192809b523ab80486bb10f237f6b9bf49bc5b2
21A95A0425 <109516179+21A95A0425@users.noreply.github.com>
2022-09-16 00:05:55 +0530

    a=int(input()) if a>0:     for i in range(1,a+1):         z=0         print(" "*(a-i),end="")         j=2         for k in range(1,2*i):             if k<=i:                 print(k,end="")             else:                 print(k-j,end="")                 j+=2         print()

--- Pattern.py
+++ Pattern.py
@@ -1,5 +1,13 @@
 a=int(input())
-for i in range(1,a+1):
-    for j in range(1,a+1):
-        print(i,end=" ")
-    print()
+if a>0:
+    for i in range(1,a+1):
+        z=0
+        print(" "*(a-i),end="")
+        j=2
+        for k in range(1,2*i):
+            if k<=i:
+                print(k,end="")
+            else:
+                print(k-j,end="")
+                j+=2
+        print()
