e5003902fcaa3f24252783b3b7051fe98ad3d9c1
21A95A0425 <109516179+21A95A0425@users.noreply.github.com>
2022-09-15 23:08:47 +0530

    z=int(input()) for i in range(1,z+1):     for j in range(1,1+z):         if i==z:             print("*",end="")             continue         if i==j or j==1:             print("*",end="")         else:             print(end=" ")     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,6 +1,11 @@
-a=int(input())
-for j  in range(1,a+1,1):
-    for i in range(a,0,-1):
-        print(i,end=" ")
-    print("")
-    
+z=int(input())
+for i in range(1,z+1):
+    for j in range(1,1+z):
+        if i==z:
+            print("*",end="")
+            continue
+        if i==j or j==1:
+            print("*",end="")
+        else:
+            print(end=" ")
+    print()
