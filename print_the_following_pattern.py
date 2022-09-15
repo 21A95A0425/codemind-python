cd91891ce44260efb1d78507ff24a8bc99d33f24
21A95A0425 <109516179+21A95A0425@users.noreply.github.com>
2022-09-15 23:46:09 +0530

    a= int(input()) for i in range(1,a+1):     for j in range(1,i+1):         print(j,end="")     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,6 +1,5 @@
-a=int(input())
-b={1:"A",2:"B",3:"C",4:"D",5:"E",6:"F",7:"G",8:"H"}
+a= int(input())
 for i in range(1,a+1):
-    for k in range(1,a+1):
-        print(b[i],end=" ")
+    for j in range(1,i+1):
+        print(j,end="")
     print()
