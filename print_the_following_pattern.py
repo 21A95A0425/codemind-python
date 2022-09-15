df6d54cb655573f9baef7bbdba4250fc3d2aa542
21A95A0425 <109516179+21A95A0425@users.noreply.github.com>
2022-09-15 23:50:52 +0530

    z=int(input()) a=z m={1:"A",2:"B",3:"C",4:"D",5:"E",6:"F",7:"G",8:"H"} for i in range(1,z+1):     for k in range(1,a+1):         print(m[a],end=" ")     a-=1     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,8 @@
-a= int(input())
-for i in range(1,a+1):
-    for j in range(1,i+1):
-        print(j,end="")
+z=int(input())
+a=z
+m={1:"A",2:"B",3:"C",4:"D",5:"E",6:"F",7:"G",8:"H"}
+for i in range(1,z+1):
+    for k in range(1,a+1):
+        print(m[a],end=" ")
+    a-=1
     print()
