526e8b486bc2832619276aa363e6bc27553075dd
21A95A0425 <109516179+21A95A0425@users.noreply.github.com>
2022-09-16 00:01:55 +0530

    z=int(input()) d={1:"A",2:"B",3:"C",4:"D",5:"E",6:"F",7:"G",8:"H"} if z>0:     for i in range(1,z+1):         a=0         print(" "*(z-i),end="")         j=2         for k in range(1,2*i):             if k<=i:                 print(d[k],end="")             else:                 print(d[k-j],end="")                 j+=2         print()

--- Print_the_following_pattern.py
+++ Print_the_following_pattern.py
@@ -1,8 +1,14 @@
-a=int(input())
-for i in range(1,a+1):
-    for j in range(1,a+1):
-        if i==j or (j==a+1-i):
-            print(a+1-i,end=" ")
-        else:
-            print(end=" ")
-    print()
+z=int(input())
+d={1:"A",2:"B",3:"C",4:"D",5:"E",6:"F",7:"G",8:"H"}
+if z>0:
+    for i in range(1,z+1):
+        a=0
+        print(" "*(z-i),end="")
+        j=2
+        for k in range(1,2*i):
+            if k<=i:
+                print(d[k],end="")
+            else:
+                print(d[k-j],end="")
+                j+=2
+        print()
