b970707be96361e3bc23fefdc6dd33a141171131
21A95A0425 <109516179+21A95A0425@users.noreply.github.com>
2022-09-16 11:47:57 +0530

    z=int(input()) s=0 a=[] for i in range(1,z+1):     m=int(input())     a.append(m) t=int(input()) for k in range(len(a)):     if a[k]<=t:         s=s+1     else:         s+=2 print(s)

---
+++ Airport__authority.py
@@ -0,0 +1,13 @@
+z=int(input())
+s=0
+a=[]
+for i in range(1,z+1):
+    m=int(input())
+    a.append(m)
+t=int(input())
+for k in range(len(a)):
+    if a[k]<=t:
+        s=s+1
+    else:
+        s+=2
+print(s)
