7f972335315cc609734449b4bfe4f5983f4e8f02
DURGA NAGA V SATYA KARTHIK KONDA <109516179+21A95A0425@users.noreply.github.com>
2022-09-23 23:27:54 +0530

    z=int(input()) a=list(map(int,input().split())) b=a.copy() b.sort() if b[0]!=b[-1]:     for i in range(len(a)):         if a[i]==b[-1]:             if i==z-1:                 print(1)             else:                 print(2)             break else:     print(0)

---
+++ Make_Array_Elements_Equal.py
@@ -0,0 +1,14 @@
+z=int(input())
+a=list(map(int,input().split()))
+b=a.copy()
+b.sort()
+if b[0]!=b[-1]:
+    for i in range(len(a)):
+        if a[i]==b[-1]:
+            if i==z-1:
+                print(1)
+            else:
+                print(2)
+            break
+else:
+    print(0)
