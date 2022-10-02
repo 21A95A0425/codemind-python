045cd9ab595f25d5b4063135c8645bd1244aba53
DURGA NAGA V SATYA KARTHIK KONDA <109516179+21A95A0425@users.noreply.github.com>
2022-10-02 18:53:02 +0530

    z=int(input()) m=list(map(int,input().strip().split())) c=1 a=1 b=1 d=0 e=0 for i in range(len(m)):     e+=1     a=b     b=c     c=m[i]     if a%2==0 and c%2==0:         if b%2==0:             d+=1 print(d)

---
+++ count_even_numbers_between_even_numbers.py
@@ -0,0 +1,16 @@
+z=int(input())
+m=list(map(int,input().strip().split()))
+c=1
+a=1
+b=1
+d=0
+e=0
+for i in range(len(m)):
+    e+=1
+    a=b
+    b=c
+    c=m[i]
+    if a%2==0 and c%2==0:
+        if b%2==0:
+            d+=1
+print(d)
