8052176afa88cf14d0c13fae03d055e452c5accc
DURGA NAGA V SATYA KARTHIK KONDA <109516179+21A95A0425@users.noreply.github.com>
2022-10-02 18:50:08 +0530

    z=int(input()) m=list(map(int,input().strip().split())) c=0 a=0 b=0 d=0 e=0 for i in range(len(m)):     e+=1     a=b     b=c     c=m[i]     if a%2 and c%2:         if b%2:             d+=1 print(d)

---
+++ count_the_add_numbers_inbetween_odd_numbers.py
@@ -0,0 +1,16 @@
+z=int(input())
+m=list(map(int,input().strip().split()))
+c=0
+a=0
+b=0
+d=0
+e=0
+for i in range(len(m)):
+    e+=1
+    a=b
+    b=c
+    c=m[i]
+    if a%2 and c%2:
+        if b%2:
+            d+=1
+print(d)
