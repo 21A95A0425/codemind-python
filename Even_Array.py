8bae381e4b244f41687995bf4f6b156f891f1d07
DURGA NAGA V SATYA KARTHIK KONDA <109516179+21A95A0425@users.noreply.github.com>
2022-10-02 19:45:50 +0530

    z=int(input()) m=list(map(int,input().strip().split())) for i in range(len(m)):     if m[i]%2==0:         flag=True     else:         flag=False         print("False")         break if flag==True:     print("True")

---
+++ Even_Array.py
@@ -0,0 +1,11 @@
+z=int(input())
+m=list(map(int,input().strip().split()))
+for i in range(len(m)):
+    if m[i]%2==0:
+        flag=True
+    else:
+        flag=False
+        print("False")
+        break
+if flag==True:
+    print("True")
