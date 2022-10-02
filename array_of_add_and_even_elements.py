4b41f04a0e10813a91f180747f4ed8158ee3b5a0
DURGA NAGA V SATYA KARTHIK KONDA <109516179+21A95A0425@users.noreply.github.com>
2022-10-02 19:10:47 +0530

    z=int(input()) m=list(map(int,input().strip().split())) a=[] b=[] for i in range(len(m)):     if m[i]%2!=0:         a.append(m[i])     else:         b.append(m[i]) for j in range(len(b)):     a.append(b[j]) for k in range(len(a)):     print(a[k],end=" ")

---
+++ array_of_add_and_even_elements.py
@@ -0,0 +1,13 @@
+z=int(input())
+m=list(map(int,input().strip().split()))
+a=[]
+b=[]
+for i in range(len(m)):
+    if m[i]%2!=0:
+        a.append(m[i])
+    else:
+        b.append(m[i])
+for j in range(len(b)):
+    a.append(b[j])
+for k in range(len(a)):
+    print(a[k],end=" ")
