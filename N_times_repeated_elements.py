7a4c24669d94f018bda103c648a3bf35545e6e33
DURGA NAGA V SATYA KARTHIK KONDA <109516179+21A95A0425@users.noreply.github.com>
2022-10-02 21:31:17 +0530

    z=int(input()) m=list(map(int,input().strip().split()))[:z] k=int(input()) a=[] f=False for i in m:     if i not in a:         a.append(i)         b=m.count(i)         if b==k:             f=True             print(i,end=" ") if f==False:     print(-1)

---
+++ N_times_repeated_elements.py
@@ -0,0 +1,14 @@
+z=int(input())
+m=list(map(int,input().strip().split()))[:z]
+k=int(input())
+a=[]
+f=False
+for i in m:
+    if i not in a:
+        a.append(i)
+        b=m.count(i)
+        if b==k:
+            f=True
+            print(i,end=" ")
+if f==False:
+    print(-1)
