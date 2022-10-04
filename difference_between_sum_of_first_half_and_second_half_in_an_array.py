93846a630ebf5073ad6051edc23ac2472e713ef9
DURGA NAGA V SATYA KARTHIK KONDA <109516179+21A95A0425@users.noreply.github.com>
2022-10-04 10:22:22 +0530

    z=int(input()) a=list(map(int,input().split())) b=[] c=[] x=z//2 for i in range(x):     b.append(a[i]) for j in a:     if j not in b:         c.append(j) print(abs(sum(b)-sum(c)))

---
+++ difference_between_sum_of_first_half_and_second_half_in_an_array.py
@@ -0,0 +1,11 @@
+z=int(input())
+a=list(map(int,input().split()))
+b=[]
+c=[]
+x=z//2
+for i in range(x):
+    b.append(a[i])
+for j in a:
+    if j not in b:
+        c.append(j)
+print(abs(sum(b)-sum(c)))
