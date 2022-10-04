0bf55d98629ffcf0d0a13442e98fa1774dfe87ba
DURGA NAGA V SATYA KARTHIK KONDA <109516179+21A95A0425@users.noreply.github.com>
2022-10-04 17:26:04 +0530

    z=int(input()) m=list(map(int,input().split())) g=[] h=0 count=0 for i in range(len(m)):     c=abs(m[i])     a=str(c)     b=len(a)     if b>h:         h=b for j in range(len(m)):     c=abs(m[j])     a=str(c)     b=len(a)     if b==h:         count+=1 print(count)

---
+++ Max_digits_count.py
@@ -0,0 +1,18 @@
+z=int(input())
+m=list(map(int,input().split()))
+g=[]
+h=0
+count=0
+for i in range(len(m)):
+    c=abs(m[i])
+    a=str(c)
+    b=len(a)
+    if b>h:
+        h=b
+for j in range(len(m)):
+    c=abs(m[j])
+    a=str(c)
+    b=len(a)
+    if b==h:
+        count+=1
+print(count)
