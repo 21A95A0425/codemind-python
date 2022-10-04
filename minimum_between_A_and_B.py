3ee7580adf0d1db2572b78fa4d878535f8906f17
DURGA NAGA V SATYA KARTHIK KONDA <109516179+21A95A0425@users.noreply.github.com>
2022-10-04 10:01:12 +0530

    z=int(input()) m=list(map(int,input().split())) a,b=map(int,input().split()) s=0 c=[] for i in range(z):     if a<=m[i]<=b:         c.append(m[i])         s=1 if s==1:     print(min(c)) else:     print(-1)

---
+++ minimum_between_A_and_B.py
@@ -0,0 +1,13 @@
+z=int(input())
+m=list(map(int,input().split()))
+a,b=map(int,input().split())
+s=0
+c=[]
+for i in range(z):
+    if a<=m[i]<=b:
+        c.append(m[i])
+        s=1
+if s==1:
+    print(min(c))
+else:
+    print(-1)
