2b2b7f33b9f0c949493b8a90c31ece330f579c99
DURGA NAGA V SATYA KARTHIK KONDA <109516179+21A95A0425@users.noreply.github.com>
2022-10-04 11:03:23 +0530

    a,b=map(int,input().split()) m=list(map(int,input().split()))[:a] y=list(map(int,input().split()))[:b] a=[] b=[] c=0 for i in m:     if i not in a:         a.append(i) for j in y:     if j not in b:         b.append(j) for k in a:     if k in b:         c+=1 print(c)

---
+++ Count_elements_I.py
@@ -0,0 +1,16 @@
+a,b=map(int,input().split())
+m=list(map(int,input().split()))[:a]
+y=list(map(int,input().split()))[:b]
+a=[]
+b=[]
+c=0
+for i in m:
+    if i not in a:
+        a.append(i)
+for j in y:
+    if j not in b:
+        b.append(j)
+for k in a:
+    if k in b:
+        c+=1
+print(c)
