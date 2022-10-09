0e32e7c5f03f7bab52d65901041c7808e15e2099
DURGA NAGA V SATYA KARTHIK KONDA <109516179+21A95A0425@users.noreply.github.com>
2022-10-09 10:52:47 +0530

    z=input() z=z.casefold() a=z.split() d='' if len(a)==1:     print(0) else:     b=a[0]     for i in b:         f=False         for k in a:             if i in k:                 f=True             else:                 f=False                 break         if f==True:             d=d+i             for j in a:                 for p in range(len(j)):                     if j[p]==i:                         j=j[:p]+j[p+1:]                         break     print(len(d))

---
+++ Common_characters_-_2.py
@@ -0,0 +1,24 @@
+z=input()
+z=z.casefold()
+a=z.split()
+d=''
+if len(a)==1:
+    print(0)
+else:
+    b=a[0]
+    for i in b:
+        f=False
+        for k in a:
+            if i in k:
+                f=True
+            else:
+                f=False
+                break
+        if f==True:
+            d=d+i
+            for j in a:
+                for p in range(len(j)):
+                    if j[p]==i:
+                        j=j[:p]+j[p+1:]
+                        break
+    print(len(d))
