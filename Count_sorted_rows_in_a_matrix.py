ad103cd7c70dbf72456802ebcaccdc4cad329ccb
DURGA NAGA V SATYA KARTHIK KONDA <109516179+21A95A0425@users.noreply.github.com>
2022-10-09 11:54:46 +0530

    z,m=map(int,input().split()) c=0 p=[] for i in range(z):     a=list(map(int,input().split()))     b=a.copy()     b.sort()     q=b[::-1]     if a==b or a==q:         c+=1 print(c)

---
+++ Count_sorted_rows_in_a_matrix.py
@@ -0,0 +1,11 @@
+z,m=map(int,input().split())
+c=0
+p=[]
+for i in range(z):
+    a=list(map(int,input().split()))
+    b=a.copy()
+    b.sort()
+    q=b[::-1]
+    if a==b or a==q:
+        c+=1
+print(c)
