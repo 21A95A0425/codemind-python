6a0b81465d76e84e7a05298baab8dbfbb4eccfc5
DURGA NAGA V SATYA KARTHIK KONDA <109516179+21A95A0425@users.noreply.github.com>
2022-10-09 11:33:53 +0530

    z,m=map(int,input().split()) s=[0]*(m) for i in range(z):     m=list(map(int,input().split())) #    b.append(sum(a))     for j in range(len(m)):         s[j]+=m[j] for k in s:     print(k,end=' ')

---
+++ Column_wise_sum_of_a_matrix.py
@@ -0,0 +1,9 @@
+z,m=map(int,input().split())
+s=[0]*(m)
+for i in range(z):
+    m=list(map(int,input().split()))
+#    b.append(sum(a))
+    for j in range(len(m)):
+        s[j]+=m[j]
+for k in s:
+    print(k,end=' ')
