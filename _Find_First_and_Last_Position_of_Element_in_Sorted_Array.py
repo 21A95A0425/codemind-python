df48fc59c99433af318ada50d08c9e6a53c65c0a
DURGA NAGA V SATYA KARTHIK KONDA <109516179+21A95A0425@users.noreply.github.com>
2022-11-01 10:35:25 +0530

    n=int(input()) m=list(map(int,input().split())) a=int(input()) f=True p,q=False,False for i in range(len(m)):     if m[i]==a and f==True:         b=i         f=False         p=True     if m[i]==a and f==False:         c=i         q=True if p==False:         b=-1 if q==False:         c=-1 print(b,c)

---
+++ _Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
@@ -0,0 +1,18 @@
+n=int(input())
+m=list(map(int,input().split()))
+a=int(input())
+f=True
+p,q=False,False
+for i in range(len(m)):
+    if m[i]==a and f==True:
+        b=i
+        f=False
+        p=True
+    if m[i]==a and f==False:
+        c=i
+        q=True
+if p==False:
+        b=-1
+if q==False:
+        c=-1
+print(b,c)
