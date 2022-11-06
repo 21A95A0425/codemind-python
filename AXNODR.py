2cf5d7169406a954bdff6271ca5d755491063029
DURGA NAGA V SATYA KARTHIK KONDA <109516179+21A95A0425@users.noreply.github.com>
2022-11-06 09:43:37 +0530

    for ashok in range(int(input())):     N = int(input())     if((N-0)%4 ==0):         print(N+3)     elif((N-1)%4==0):         print(N)     elif((N-2)%4 == 0 or (N-3)%4 == 0):         print(3)

---
+++ AXNODR.py
@@ -0,0 +1,8 @@
+for ashok in range(int(input())):
+    N = int(input())
+    if((N-0)%4 ==0):
+        print(N+3)
+    elif((N-1)%4==0):
+        print(N)
+    elif((N-2)%4 == 0 or (N-3)%4 == 0):
+        print(3)
